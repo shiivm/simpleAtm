8c88adf1ffea52b65c640048d700be02232788cc
shivam_mehta <shivam.mehta@simplilearn.com>
2020-05-24 01:44:15 +0530

    deposit and withdraw functionality added

--- Atm.py
+++ Atm.py
@@ -1,10 +1,14 @@
 import csv
 import time
+import os
 
 class Atm:
 
-    def __init__(self,acc_no):
-        self.acc_no = acc_no
+    def __init__(self,row):
+        self.name = row['name']
+        self.acc_no = row['accountNo']
+        self.balance = float(row['balance'])
+        self.filename = 'data.csv'
     
     def menu(self):
         print("\n 1.Balance Enquiry \t 2.Withdraw \n 3.Deposit \t\t 4.Exit\n" )
@@ -26,12 +30,66 @@ class Atm:
             self.menu()
 
     def balance_enquiry(self):
-        file = open('data.csv','r')       
-        data = csv.reader(file)
-        for row in data:
-            if(row[1] == self.acc_no):
-                print("\nYour Account Balance is : {}".format(row[3]))
+        print("\nYour Account Balance is : {}".format(self.balance))
         time.sleep(1)
         self.menu()
-        
-        
+
+    def withdraw(self):
+        amount = float(input("Enter amount: "))
+        if(amount > self.balance):
+            print("Insufficient balance")
+            self.menu()
+        else:
+            self.balance  -= float(amount)
+            mylist = self.prepaer_update_data(self.balance); 
+            self.update_file(mylist)
+            print('Please collect the cash')
+            self.menu()
+
+    def deposit(self):
+        amount = float(input('Please enter amont to be deposited: '))
+        if amount <= 0:
+            print('Please enter amount greater than 0')
+            self.deposit()
+        else:
+            self.balance  += float(amount)
+            mylist = self.prepaer_update_data(self.balance); 
+            self.update_file(mylist)
+            print('Money deposited successfully')
+            self.menu()
+
+    def prepaer_update_data(self,amount):
+        mylist = []; 
+        fieldnames = ['name', 'accountNo','pin','balance']
+        try:
+            with open(self.filename,'r+', newline='') as file:
+                data = csv.DictReader(file)
+                writer = csv.DictWriter(file,fieldnames=fieldnames)
+                for row in data:
+                    if row['accountNo'] == self.acc_no:
+                        mylist.append({'name' : row['name'],'accountNo' : row['accountNo'],'pin':row['pin'],'balance' : float(amount)})
+                    else:
+                        mylist.append(row)
+        except:
+            pass
+
+        return mylist
+    
+    def update_file(self,data):
+        if(len(data) == 0):
+            return
+        tempfilename = 'temp.csv'
+        fieldnames = ['name', 'accountNo','pin','balance']
+        try:
+            os.remove(tempfilename)  # delete any existing temp file
+        except OSError:
+            pass
+        try:
+            with open(tempfilename,'a', newline='') as file:
+                writer = csv.DictWriter(file,fieldnames=fieldnames)
+                writer.writeheader()
+                for item in data:
+                    writer.writerow(item)
+            os.rename(tempfilename,self.filename)
+        except:
+            pass
--- Auth.py
+++ Auth.py
@@ -8,32 +8,31 @@ class Auth:
     def login(self):
         acc_no = input('Enter your account number:')
         pin = input('Enter your atm pin:')
-        file = open('data.csv','r')       
-        data = csv.reader(file)
-        for row in data:
-            if row[1]== acc_no and row[2] == pin:
-                print("\nWelcom {}".format(row[0]))
-                atm = Atm(acc_no)
-                atm.menu()
-        else:
-            print('Invalid credentials')
-            self.login()
-        file.close()
+        with open('data.csv','r', newline='') as file:
+            data = csv.DictReader(file)
+            for row in data:
+                if row['accountNo']== acc_no and row['pin'] == pin:
+                    print("\nWelcom {}".format(row['name']))
+                    atm = Atm(row)
+                    atm.menu()
+            else:
+                print('Invalid credentials')
+                self.login()
 
     def register(self):
         name = input('Enter your name:')
         acc_no = input('Enter your account number:')
         pin = input('Enter your pin:')
         bal = 0
-        fields = [name,acc_no,pin,bal] 
-        file = open('data.csv','a')  
-        writer = csv.writer(file)
-        writer.writerow(fields)
-        sys.stdout.write("Registering")
-        for i in range(6):
-            sys.stdout.write('.')
-            sys.stdout.flush()
-            time.sleep(.6)
+        with open('data.csv','a', newline='') as file:
+            fieldnames = ['name', 'accountNo','pin','balance']
+            writer = csv.DictWriter(file,fieldnames=fieldnames)
+            writer.writerow({'name':name,'accountNo':acc_no,'pin':pin,'balance':bal})
+            sys.stdout.write("Registering")
+            for i in range(6):
+                sys.stdout.write('.')
+                sys.stdout.flush()
+                time.sleep(.6)
         print('\n')
         self.login()
 
